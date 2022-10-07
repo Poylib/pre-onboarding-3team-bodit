2f71fa797fab9682b2d0ae13c9c0bb7b07eacfee
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-10-07 23:21:53 +0900

    Add: 그래프 데이터 현재날짜기준 1시간 간격으로 평균내서 나타내기 완료

--- src/components/GraphField/GraphField.jsx
+++ src/components/GraphField/GraphField.jsx
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 import Graph from './Graph';
 
 const GraphField = () => {
-  const [tempData, setTempData] = useState({});
-  const [humidityData, setHumidityData] = useState({});
-  const [pressureData, setPressureData] = useState({});
+  const [tempData, setTempData] = useState([]);
+  const [humidityData, setHumidityData] = useState([]);
+  const [pressureData, setPressureData] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -14,11 +14,10 @@ const GraphField = () => {
       try {
         const {
           data: { channel, feeds },
-        } = await axios.get('https://api.thingspeak.com/channels/1348864/feeds.json?api_key=6SKW0U97IPV2QQV9');
-
-        setTempData({ id: channel.field1, data: extract('field1', feeds) });
-        setHumidityData({ id: channel.field2, data: extract('field2', feeds) });
-        setPressureData({ id: channel.field3, data: extract('field3', feeds) });
+        } = await axios.get(`https://api.thingspeak.com/channels/1348864/feeds.json?api_key=6SKW0U97IPV2QQV9&${makeDateRangeQuery()}&results=140&average=60&status=true`);
+        setTempData([{ id: channel.field1, data: extract('field1', feeds) }]);
+        setHumidityData([{ id: channel.field2, data: extract('field2', feeds) }]);
+        setPressureData([{ id: channel.field3, data: extract('field3', feeds) }]);
       } catch (error) {
         console.log(error);
         alert('통신 실패');
@@ -28,7 +27,7 @@ const GraphField = () => {
   }, []);
 
   const extract = (graphType, allData) => {
-    const graphValue = [];
+    let graphValue = [];
 
     allData.map(allDataByDate => {
       const graphX = allDataByDate.created_at.substr(11, 5);
@@ -43,19 +42,44 @@ const GraphField = () => {
     return graphValue;
   };
 
+  const makeDateRangeQuery = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const dateStr = year + '-' + month + '-' + day;
+
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+    const timeStr = hours + ':' + minutes + ':' + seconds;
+
+    const startTime = `start=${dateStr}%2000:00:00`;
+    const endTime = `end=${dateStr}%20${timeStr}`;
+    return startTime + '&' + endTime;
+  };
+
   return (
     <>
-      <div style={{ height: '500px' }}>
+      <GraphWrapper>
         <Graph data={tempData} />
-      </div>
-      <div style={{ height: '500px' }}>
+      </GraphWrapper>
+      <GraphWrapper>
         <Graph data={humidityData} />
-      </div>
-      <div style={{ height: '500px' }}>
+      </GraphWrapper>
+      <GraphWrapper>
         <Graph data={pressureData} />
-      </div>
+      </GraphWrapper>
     </>
   );
 };
 
 export default GraphField;
+
+const GraphWrapper = styled.div`
+  height: 500px;
+
+  text {
+    font-size: 20px;
+  }
+`;
