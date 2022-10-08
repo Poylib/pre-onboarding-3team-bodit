69ff3fb87394e28cc645c131b2afdc7eb9b76740
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-10-08 21:23:29 +0900

    Modify: 필요없는 코드 제거

--- src/components/GraphField/GraphField.jsx
+++ src/components/GraphField/GraphField.jsx
@@ -78,18 +78,15 @@ const GraphFieldWrapper = styled.div`
   border: 3px solid ${blue};
   background-color: #ffffffd5;
 
-
-
-  
   @media screen and (max-width: 1024px) {
     display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 100%;
-  height: 100%;
-  padding: 9%  0px  9%;
-  border: 3px solid ${blue};
-  background-color: #ffffffd5;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 100%;
+    height: 100%;
+    padding: 9% 0px 9%;
+    border: 3px solid ${blue};
+    background-color: #ffffffd5;
   }
   @media screen and (max-width: 890px) {
   }
